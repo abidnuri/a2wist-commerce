5366b191d88513c0235a5e20d96fbd66cf47ba59
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-07-18 15:21:39 +0600

    navigation done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Navbars from './Components/Header/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbars/>
+      <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+    </Router>  
+      </div>
   );
 }
 
