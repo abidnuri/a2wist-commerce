b72907ba7931a43f57c58a884110b907e7b6755c
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-07-18 15:48:42 +0600

    home page done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import fackData from '../../fackeData/fackData.json'
+import Product from '../Product/Product';
+
 
 const Home = () => {
+
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const products =  fackData
+        setProducts(products)
+    }, [])
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className="container">
+        <div className="row">
+            {
+                products.map(product => <Product key={product.id} product={product}></Product>)
+            }
+        </div>
         </div>
     );
 };
