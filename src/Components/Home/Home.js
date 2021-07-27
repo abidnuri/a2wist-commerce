e924e1a3614c13c20d82d57b78caed1a9e99f16f
abidnuri <mdabidnuri@gmail.com>
2021-07-27 17:52:37 +0600

    home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,21 +3,17 @@ import FeaturedProduct from '../FeaturedProduct/FeaturedProduct';
 import Slider from '../Slider/Slider';
 import HotProducts from './../HotProducts/HotProducts';
 import Discount from '../Discount/Discount';
-import Footer from '../Shared/Footer/Footer';
-import Navbar from '../Shared/Navbar/Navbar';
 import SingleProduct from '../SingleProduct/SingleProduct';
 
 const Home = () => {
     return (
-        <div>
-            <Navbar />
+        <>
             <Slider />
             <FeaturedProduct />
-            <SingleProduct/>
+            <SingleProduct />
             <HotProducts />
             <Discount />
-            <Footer />
-        </div>
+        </>
     );
 };
 
