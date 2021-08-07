c6436f78d708b3310114b4ee1738dde178e3865b
sajid73 <smsajid7300@gmail.com>
2021-08-07 18:20:22 +0600

    router problem fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home/Home";
 import PrivateRoute from "./components/Login/PrivateRoute/PrivateRoute";
 import Signup from "./components/Login/Signup";
 import NotFound from "./components/NotFound/NotFound";
-import ProductDetail from './components/ProductDetail/ProductDetail';
+import ProductDetail from "./components/ProductDetail/ProductDetail";
 import Review from "./components/Review/Review";
 import Footer from "./components/Shared/Footer/Footer";
 import Navbar from "./components/Shared/Navbar/Navbar";
@@ -20,17 +20,17 @@ function App() {
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
         <Navbar />
-        <Switch>
-          <div className="mt-20">
-          <Route exact path="/" component={Home} />
-          <Route path="/signup" component={Signup} />
-          <PrivateRoute path="/contact" component={Contact} />
-          <Route path="/review" component={Review} />
-          <Route path="/products" component={ShopPage} />
-          <Route path="/product/:productid" component={ProductDetail} />
-          <Route path="*" component={NotFound} />
-          </div>
-        </Switch>
+        <div className="mt-20">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute path="/contact" component={Contact} />
+            <Route path="/review" component={Review} />
+            <Route path="/products" component={ShopPage} />
+            <Route path="/product/:productid" component={ProductDetail} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
         <Footer />
       </Router>
     </UserContext.Provider>
