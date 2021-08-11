d7409010139c00dfd64d34c87d9a70f077da5f97
abidnuri <mdabidnuri@gmail.com>
2021-08-11 15:22:11 +0600

    placeholder

--- src/Components/Login/Signup.js
+++ src/Components/Login/Signup.js
@@ -72,6 +72,7 @@ const Signup = () => {
                 id="email"
                 required
                 name="email"
+                placeholder="Enter your email"
                 value={email}
                 onChange={e => setEmail(e.target.value)}
                 className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
