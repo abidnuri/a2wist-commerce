ee90ad123de62fcfbb73c198dd206f8fbd266bad
abidnuri <mdabidnuri@gmail.com>
2021-08-12 15:59:23 +0600

    redux state user with token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Contact from "./components/Contact/Contact";
@@ -13,27 +13,52 @@ import ShopPage from "./components/ShopPage/ShopPage";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SignupComplete from './components/Login/SignupComplete/SignupComplete';
+import { auth } from "./components/Login/firebase.config";
+import { useDispatch } from "react-redux";
 
 
 function App() {
+  const dispatch = useDispatch();
+
+  // checking the firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user){
+        const idTokenResult = await user.getIdToken()
+        
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult,
+          }
+          
+        })
+      }
+    })
+
+    //cleaning 
+    return () => unsubscribe();
+  }, [])
+
   return (
-      <Router>
-        <Navbar />
-        <ToastContainer />
-        <div className="mt-20">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/review" component={Review} />
-            <Route path="/products" component={ShopPage} />
-            <Route path="/product/:productid" component={ProductDetail} />
-            <Route path="/complete" component={SignupComplete} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
+    <Router>
+      <Navbar />
+      <ToastContainer />
+      <div className="mt-20">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/review" component={Review} />
+          <Route path="/products" component={ShopPage} />
+          <Route path="/product/:productid" component={ProductDetail} />
+          <Route path="/complete" component={SignupComplete} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
